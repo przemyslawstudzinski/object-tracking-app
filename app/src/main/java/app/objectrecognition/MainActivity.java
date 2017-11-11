80ea85d0518630b7ae9c02a2c55b825888aa2666
przstudzinski <przemyslawstudzinski1993@gmail.com>
2017-11-11 15:55:26 +0100

    Added OpenCV library

--- app/src/main/java/app/objectrecognition/MainActivity.java
+++ app/src/main/java/app/objectrecognition/MainActivity.java
@@ -2,6 +2,9 @@ package app.objectrecognition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
     }
 }
